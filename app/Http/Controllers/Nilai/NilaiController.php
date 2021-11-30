c5b85fa97fce44379a10b4efc1bb5a8c04644f75
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2021-11-30 15:38:03 +0700

    itungan

--- app/Http/Controllers/Nilai/NilaiController.php
+++ app/Http/Controllers/Nilai/NilaiController.php
@@ -101,6 +101,7 @@ class NilaiController extends CustomController
                     ]);
 
                 $availableSubjects = [];
+                //getting available subjects in interest
                 foreach ($tmpAvailableSubjects as $valueSubject) {
                     $subject_id = $valueSubject->mapel->id;
                     $tmpAvailableSubject['id'] = $subject_id;
@@ -109,7 +110,9 @@ class NilaiController extends CustomController
                         ->where('id_mapel', $subject_id)
                         ->first();
                     $score = 0;
-                    $score_index = 'rendah';
+                    $score_indicator = 'rendah';
+                    $score_index = [];
+                    $score_formula = [];
                     if ($single_score) {
                         $score = $single_score->nilai;
                     }
@@ -119,56 +122,118 @@ class NilaiController extends CustomController
                             ->where('indikator', $indicator)
                             ->where('id_mapel', $subject_id)
                             ->first();
-                        if(!$subject_indicator) {
+                        if (!$subject_indicator) {
                             return $this->jsonResponse('Indikator Batas Mapel Belum Di Tentukan', 202);
                         }
                         $bawah = $subject_indicator->bawah;
                         $tengah = $subject_indicator->tengah;
                         $atas = $subject_indicator->atas;
-                        if($indicator === 'rendah' && $score < $tengah) {
-                            $score_index = 'rendah';
+                        if ($indicator === 'rendah' && $score < $tengah) {
+                            $score_indicator = 'rendah';
+                            array_push($score_index, 0);
+                            array_push($score_formula, 0);
                             break;
-                        } else if($indicator === 'cukup' && ($score >= $bawah && $score <= $atas) ) {
-                            $score_index = 'cukup';
+                        } else if ($indicator === 'cukup' && ($score >= $bawah && $score <= $atas)) {
+                            $score_indicator = 'cukup';
+                            $middle_indicator = round(($atas - $score) / ($atas - $bawah), 1, PHP_ROUND_HALF_UP);
+                            $top_indicator = round(($score - $bawah) / ($atas - $bawah), 1, PHP_ROUND_HALF_UP);
+                            array_push($score_index, $top_indicator);
+                            array_push($score_index, $middle_indicator);
+                            array_push($score_formula, '(' . $score . ' - ' . $bawah . ') / (' . $atas . ' - ' . $bawah . ') = (' . ($score - $bawah) . ' / ' . ($atas - $bawah) . ') = ' . $top_indicator);
+                            array_push($score_formula, '(' . $atas . ' - ' . $score . ') / (' . $atas . ' - ' . $bawah . ') = (' . ($atas - $score) . ' / ' . ($atas - $bawah) . ') = ' . $middle_indicator);
                             break;
-                        }else if($indicator === 'tinggi' && $score > $tengah) {
-                            $score_index = 'tinggi';
+                        } else if ($indicator === 'tinggi' && $score > $tengah) {
+                            $score_indicator = 'tinggi';
+                            array_push($score_index, 1);
+                            array_push($score_formula, 1);
                             break;
                         }
                     }
                     $tmpAvailableSubject['score'] = $score;
+                    $tmpAvailableSubject['score_indicator'] = $score_indicator;
                     $tmpAvailableSubject['score_index'] = $score_index;
+                    $tmpAvailableSubject['score_formula'] = $score_formula;
                     array_push($availableSubjects, $tmpAvailableSubject);
                 }
-                $tmpInterest['subjects'] = $availableSubjects;
 
+                $tmpInterest['subjects'] = $availableSubjects;
+                $tmpQueries = [];
+                foreach ($availableSubjects as $avaSub) {
+                    $tmpQuery['subject'] = $avaSub['id'];
+                    $tmpQuery['indicator'] = $avaSub['score_indicator'];
+                    array_push($tmpQueries, $tmpQuery);
+                }
+                $countSubject = count($availableSubjects) - 1;
+
+                //get rule
+                $rule = DB::table('rules_indicator')
+                    ->select('rules_indicator.id',
+                        'rules_indicator.rule_id',
+                        'rules_indicator.mapel_id',
+                        'rules.dudi_id',
+                        'rules.name',
+                        'rules.id',
+                        'rules.percentage',
+                        DB::raw('COUNT(rules_indicator.rule_id) as v_r'))
+                    ->join('rules', function ($join) use ($id) {
+                        $join->on('rules_indicator.rule_id', '=', 'rules.id')
+                            ->where('rules.dudi_id', $id);
+                    })
+                    ->where(function ($q) use ($tmpQueries) {
+                        foreach ($tmpQueries as $qr) {
+                            $q->orWhere(function ($qs) use ($qr) {
+                                $qs->where('rules_indicator.mapel_id', '=', $qr['subject'])
+                                    ->where('rules_indicator.value', '=', $qr['indicator']);
+                            });
+                        }
+                    })
+                    ->groupBy('rules_indicator.rule_id')
+                    ->having('v_r', '>', $countSubject)
+                    ->first();
+                $tmpInterest['rule']['name'] = $rule->name;
+                $tmpInterest['rule']['percentage'] = $rule->percentage;
+                if (!$rule) {
+                    return $this->jsonResponse('Rule Tidak Di Temukan Di ' . $value->dudi->dudi->nama, 500);
+                }
+                $indexRule = $rule->percentage;
+//                $subjects_limit = [];
+                $defuzzifikasi = [];
+                //trapesium
+                if($indexRule === 'tinggi') {
+                    foreach ($availableSubjects as $avaSub) {
+                        $subject_limit  = MapelIndicator::where('id_mapel', $avaSub['id'])
+                            ->where('indikator', $indexRule)
+                            ->first();
+                        $bawah = $subject_limit->bawah;
+                        $tengah = $subject_limit->tengah;
+                        $atas = $subject_limit->atas;
+                        $scoreRule = $avaSub['score_index'][0];
+
+                        //segitga
+                        $L1 = ($tengah - $bawah) * $scoreRule / 2;
+                        $L2 = ($atas - $tengah) * $scoreRule;
+                        $tmpLuas['luas'] = [$L1, $L2];
+                        $tmpLuas['nilai'] = $avaSub['score'];
+                        array_push($defuzzifikasi, $tmpLuas);
+                    }
+                }
+                $tmpInterest['defuzzifikasi'] = $defuzzifikasi;
+                $summary_score = 0;
+                $summary_divider = 0;
+                foreach ($defuzzifikasi as $defuzzi) {
+                    $nilai = $defuzzi['nilai'];
+                    foreach ($defuzzi['luas'] as $dl) {
+                        $summary_score += $nilai * $dl;
+                        $summary_divider += $dl;
+                    }
+                }
+                $luasan = 0;
+                if($summary_divider > 0) {
+                    $luasan = $summary_score / $summary_divider;
+                }
+                $tmpInterest['Luasan'] = round($luasan, 2, PHP_ROUND_HALF_UP);
                 array_push($tmpInterests, $tmpInterest);
             }
-
-//            $availableMapel = RulesIndicator::with(['mapel:id,nama', 'rules:id,dudi_id'])
-//                ->whereHas('rules', function ($q) {
-//                    $q->where('dudi_id', '=', 8);
-//                })
-//                ->groupBy('mapel_id')
-//                ->get([
-//                    'id', 'rule_id', 'mapel_id'
-//                ]);
-//            $tmpAvailableSubjects = [];
-//            foreach ($availableMapel as $value) {
-//                $tmpAvailableSubject['id'] = $value->mapel->id;
-//                $tmpAvailableSubject['name'] = $value->mapel->nama;
-//                array_push($tmpAvailableSubjects, $tmpAvailableSubject);
-//            }
-            $rules = Rules::with(['indicator:rule_id,mapel_id', 'indicator.mapel:id,nama,alias', 'dudi' => function ($query) {
-                $query->select('id', 'nama');
-            }])
-                ->where('dudi_id', 8)
-                ->get([
-                    'id',
-                    'dudi_id',
-                    'name',
-                    'percentage'
-                ]);
             return $this->jsonResponse('success', 200, [
 //                'score' => $tmpScores,
                 'interest' => $tmpInterests,
